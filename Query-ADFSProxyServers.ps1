a759a55b04a7d9c87a2f1fa43b58a5c00c4591b3
Can Dagdelen <can.dagdelen@microsoft.com>
2021-09-19 21:12:19 +0300

    Rename PS1

--- Query-ADFSProxyServers.ps1
+++ Query-ADFSProxyServers.ps1
@@ -35,11 +35,12 @@
 .DESCRIPTION 
  Query WAP / ADFS Proxy Servers for ADFS 
 
- #> 
+ #>
+ [cmdletBinding(DefaultParameterSetName= '')]
+ param (
+[string]$ExportPath
+  ) 
  Import-Module ADFS
-$exportlocation= "c:\script"
-if (Test-Path "$exportlocation\2012WAPList.txt") {Remove-Item "2012WAPList.txt"}
-
 
 
 try {$fblevel= (Get-AdfsFarmInformation -ErrorAction Stop).CurrentFarmBehavior}
@@ -48,14 +49,17 @@ catch {$fblevel=2} #Get-AdfsFarmInformation not available in Server 2012 R2 ADFS
     switch ($fblevel) {
     2 
         {$adfsdbname= 'AdfsConfiguration' #Server 2012R2 Farm Behavior Level
-        $dbquery = "SELECT ServiceSettingsData FROM [$adfsdbname].[IdentityServerPolicy].[ServiceSettings]"}
+        $dbquery = "SELECT ServiceSettingsData FROM [$adfsdbname].[IdentityServerPolicy].[ServiceSettings]"
+        $textoutput= "Server 2012 R2"}
     3 
         {$adfsdbname= 'AdfsConfigurationv3' #Server 2016 Farm Behavior Level
-        $dbquery = "SELECT [CertificateSubjectName], [CertificateThumbprint] FROM [$adfsdbname].[IdentityServerPolicy].[ProxyTrusts]"}
+        $dbquery = "SELECT [CertificateSubjectName], [CertificateThumbprint] FROM [$adfsdbname].[IdentityServerPolicy].[ProxyTrusts]"
+        $textoutput= "Server 2016"}
 
     4
        {$adfsdbname= 'AdfsConfigurationv4' #Server 2019 Farm Behavior Level
-        $dbquery = "SELECT [CertificateSubjectName], [CertificateThumbprint] FROM [$adfsdbname].[IdentityServerPolicy].[ProxyTrusts]"}
+        $dbquery = "SELECT [CertificateSubjectName], [CertificateThumbprint] FROM [$adfsdbname].[IdentityServerPolicy].[ProxyTrusts]"
+        $textoutput= "Server 2019"}
     }
 
 $sqlConn = "server=\\.\pipe\MICROSOFT##WID\tsql\query;database=$adfsdbname;trusted_connection=true;"
@@ -67,11 +71,30 @@ $rdr = $cmd.ExecuteReader()
 $dt = New-Object System.Data.DataTable
 $dt.Load($rdr)
 $conn.Close()
+$result = @()
+
 if ($fblevel -eq 2) {
         $strdata= $dt.ServiceSettingsData.ToString()
         $allwaps= [regex]::matches($strdata,"<d4p1:Key>CN=ADFS ProxyTrust - .{1,50}</d4p1:Key>")
-        $allwaps | foreach {$_.value -replace("<d4p1:Key>CN=ADFS ProxyTrust - ","") -replace("</d4p1:Key>") >> "$exportlocation\2012WAPList.txt"}
+        $allwaps | foreach {
+            $currentWAP= New-Object psobject
+            $currentWAPName= $_.value -replace("<d4p1:Key>CN=ADFS ProxyTrust - ","") -replace("</d4p1:Key>") 
+            $currentWAP | Add-Member -MemberType NoteProperty -name "ServerName" -value "$currentWAPName"
+            $result+= $currentWAP
+            }
         }
 
-else { $dt | Export-csv -Path "$exportlocation\2016_2019WAPList.txt" }
+else {
+        $dt | foreach {
+            $currentWAP= New-Object psobject
+            $currentWAPName= $_.CertificateSubjectName -replace("CN=ADFS ProxyTrust - ","")
+            $currentWAP | Add-Member -MemberType NoteProperty -name "ServerName" -value "$currentWAPName"
+            $currentWAP | Add-Member -MemberType NoteProperty -Name "CertificateThumbprint" -Value $_.CertificateThumbprint
+            $result+= $currentWAP
 
+            } 
+    }
+Write-Host -ForegroundColor Yellow "The Farm Behavior Level is $fblevel. The ADFS Servers are running on Windows $textoutput."
+
+$result
+if ($ExportPath) {$result | Export-Csv -Path "$ExportPath" }
